7ba3495a4a63080ac0930786117b2da6498ded38
Anna Blackwell <annablackwell@Annas-MacBook-Air.local>
2016-02-12 14:44:32 +1300

    requests in ajax file

---
+++ client/ajax.js
@@ -0,0 +1,34 @@
+import request from 'superagent'
+
+const address = {
+	Name: "Joe Blogs",
+	Address: "10 Church Street, Wellington"
+}
+
+//get request for array of addresses in database
+export const getAddresses = (callback) => {
+	request
+		.get("http://localhost:3000/addresses")
+		.end((err, res) => {
+			callback(res.body)
+			console.log('Getting addresses'))
+	}
+}
+
+//post request to localhost:3000/addresses
+export const postAddress = (address) => {
+	request
+		.post("http://localhost:3000/addresses")
+		.end((err, res) => {
+		console.log(res.body)
+			// res.body = {}
+			console.log('Submitting new address')
+	})
+}
+
+//event listener for submit button
+export const listen = () => {
+	$('#submit-button').click(() => {
+		postAddress()
+	})
+}
